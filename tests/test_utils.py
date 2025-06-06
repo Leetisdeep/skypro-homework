359dbd7e15f1ae3c6538e4a88909632940f7f12f
Leetisdeep <aaaooogjg@gmail.com>
2025-06-06 16:45:44 +0300

    changes

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,26 +1,8 @@
-from src.utils.operation_counter import operation_counter
-from src.utils.operation_search import operation_search
+import datetime as dt
+
+from src.utils import send_greeting
 
 
-def test_search_operations_by_description():
-    operations = [
-        {"description": "Перевод с карты на карту"},
-        {"description": "Открытие вклада"},
-        {"description": "Перевод организации"},
-    ]
-    result = operation_search(operations, "перевод")
-    assert len(result) == 2
-    assert result[0]["description"] == "Перевод с карты на карту"
-    assert result[1]["description"] == "Перевод организации"
-
-
-def test_count_operations_by_categories():
-    operations = [
-        {"description": "Перевод с карты на карту"},
-        {"description": "Открытие вклада"},
-        {"description": "Перевод организации"},
-    ]
-    categories = ["перевод", "вклад"]
-    result = operation_counter(operations, categories)
-    assert result["перевод"] == 2
-    assert result["вклад"] == 1
+def test_send_greeting_morning():
+    d = dt.datetime(2023, 1, 1, 8, 0, 0)
+    assert send_greeting(d) == "Доброе утро"
