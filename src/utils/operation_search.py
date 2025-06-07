44926faa3c59cfb6b8dcd08116cdbbc1ddde03f2
Leetisdeep <aaaooogjg@gmail.com>
2025-06-07 11:01:04 +0300

    deleting after making a copy

--- src/utils/operation_search.py
+++
@@ -1,7 +0,0 @@
-import re
-
-
-def operation_search(operations, search_term):
-    pattern = re.compile(re.escape(search_term), re.IGNORECASE)
-
-    return [op for op in operations if pattern.search(op.get("description", ""))]
