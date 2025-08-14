b5496a6041a342a4c4e7714bcf48e64341e2885c
Leetisdeep <aaaooogjg@gmail.com>
2025-08-14 14:22:27 +0300

    Update category.py

--- src/category.py
+++ src/category.py
@@ -48,3 +48,33 @@ class Category:
             return total / len(self.__products)
         except ZeroDivisionError:
             return 0
+
+class Smartphone(Product):
+    def __init__(self, name: str, description: str, price: float,
+                 quantity: int, efficiency: float, model: str,
+                 memory: int, color: str):
+        super().__init__(
+            name=name,
+            description=description,
+            price=price,
+            quantity=quantity
+        )
+        self.efficiency = efficiency
+        self.model = model
+        self.memory = memory
+        self.color = color
+
+
+class LawnGrass(Product):
+    def __init__(self, name: str, description: str, price: float,
+                 quantity: int, country: str, germination_period: str,
+                 color: str):
+        super().__init__(
+            name=name,
+            description=description,
+            price=price,
+            quantity=quantity
+        )
+        self.country = country
+        self.germination_period = germination_period
+        self.color = color
