6130ca9a59f45d5f62f16f1140865bb6df540380
Leetisdeep <aaaooogjg@gmail.com>
2025-08-14 14:09:04 +0300

    Update category.py

--- src/category.py
+++ src/category.py
@@ -2,8 +2,6 @@ class Category:
     """
     Класс, представляющий категорию товаров.
     """
-
-    # Атрибуты класса для подсчета количества категорий и уникальных продуктов
     category_count = 0
     product_count = 0
     _unique_products = set()
@@ -11,30 +9,39 @@ class Category:
     def __init__(self, name, description, products):
         """
         Инициализация категории.
-
-        :param name: Название категории
-        :param description: Описание категории
-        :param products: Список продуктов в категории
         """
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products  # Приватный атрибут
 
-        # Увеличиваем счетчик категорий
         Category.category_count += 1
-
-        # Добавляем продукты в множество уникальных продуктов
         for product in products:
             Category._unique_products.add(product)
-
-        # Обновляем счетчик уникальных продуктов
         Category.product_count = len(Category._unique_products)
 
+    @property
+    def products(self):
+        """Геттер для списка продуктов."""
+        return self.__products
+
+    def add_product(self, product):
+        """Добавление продукта в категорию."""
+        if isinstance(product, Product):
+            for existing_product in self.__products:
+                if existing_product == product:
+                    existing_product.quantity += product.quantity
+                    existing_product.price = max(existing_product.price, product.price)
+                    return
+            self.__products.append(product)
+            Category._unique_products.add(product)
+            Category.product_count = len(Category._unique_products)
+        else:
+            raise ValueError("Можно добавлять только объекты класса Product")
+
     def __str__(self):
-        """
-        Возвращает строковое представление категории.
-        """
-        return f"{self.name}, Остаток: {len(self.products)} шт."
+        """Строковое представление категории."""
+        total_quantity = sum(product.quantity for product in self.__products)
+        return f"{self.name}, количество продуктов: {total_quantity} шт."
 
     def __repr__(self):
-        return f"Category(name={self.name}, products={self.products})"
+        return f"Category(name={self.name}, products={self.__products})"
