94d9238c82b47facc52d70757cf80961504adabb
Leetisdeep <aaaooogjg@gmail.com>
2025-08-31 17:22:55 +0300

    Update api_client.py

--- src/api_client.py
+++ src/api_client.py
@@ -23,27 +23,27 @@ class HH(Parser):
 
     def __init__(self):
         """Инициализирует параметры запроса к API."""
-
-        self.url = 'https://api.hh.ru/vacancies'
-        self.headers = {'User-Agent': 'HH-User-Agent'}
-        self.params = {'text': '', 'page': 0, 'per_page': 100}
+        self._url = 'https://api.hh.ru/vacancies'
+        self._headers = {'User-Agent': 'HH-User-Agent'}
+        self._params = {'text': '', 'page': 0, 'per_page': 100}
         self.vacancies = []
 
     def load_vacancies(self, keyword):
         """Загружает вакансии с HeadHunter по ключевому слову."""
-
-        self.params['text'] = keyword
-        self.params['page'] = 0
-        while self.params['page'] < 20:
+        self._params['text'] = keyword
+        self._params['page'] = 0
+        
+        while self._params['page'] < 20:
             try:
-                response = requests.get(self.url, headers=self.headers, params=self.params)
+                response = requests.get(self._url, headers=self._headers, params=self._params)
                 if response.status_code == 200:
                     data = response.json()
                     if not data.get('items'):
                         break  # Если вакансий нет, прерываем цикл
+                    
                     self.vacancies.extend(data['items'])
-                    logging.info(f"Загружена страница {self.params['page']} ({len(data['items'])} вакансий)")
-                    self.params['page'] += 1
+                    logging.info(f"Загружена страница {self._params['page']} ({len(data['items'])} вакансий)")
+                    self._params['page'] += 1
                     time.sleep(0.5)  # Задержка для предотвращения блокировки API
                 else:
                     logging.error(f"Ошибка {response.status_code}: {response.text}")
